7249c61e59bbb1b8fbc2617afab9d604adf6dd14
Randy Olinger <randy.olinger@optum.com>
2020-06-11 15:46:34 -0500

    fence setup changes for arborist

--- scripts/fence_setup.sh
+++ scripts/fence_setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # entrypoint script for fence to sync user.yaml before running
 
+ARBORIST_PORT=8080
+
 sleep 2
 until (echo > /dev/tcp/postgres/5432) >/dev/null 2>&1; do
   echo "Postgres is unavailable - sleeping"
@@ -11,11 +13,11 @@ echo "postgres is ready"
 
 update-ca-certificates
 
-until curl -f -s -o /dev/null http://arborist-service/policy; do
+until curl -f -s -o /dev/null http://arborist-service:$ARBORIST_PORT/policy; do
     echo "arborist not ready, waiting..."
     sleep 10
 done
 
-fence-create sync --yaml user.yaml --arborist http://arborist-service
+fence-create sync --yaml user.yaml --arborist http://arborist-service:$ARBORIST_PORT
 
 /dockerrun.sh
